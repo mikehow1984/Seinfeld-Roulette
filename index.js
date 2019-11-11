6329b27770d0f4a949ca70fcc0307bfd755372c4
Michael Howard <mikehow1005@gmail.com>
2019-11-10 19:29:30 -0800

    converting promise chains to await-async

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const imdb = require('imdb-api');
 const clc = require('cli-color');
 const SECRETS = require('./secrets');
 
-const displayEpisode = ({season, episode, name, rating}) => {
+const displayEpisode = ({season, episode, name, rating}, showTitle) => {
     const boldSeason = clc.blue.bold(season);
     const boldEp = clc.blue.bold(episode);
     const boldTitle = clc.magenta.bold(name);
+    const boldShowTitle = clc.red.bold(showTitle);
     const itlRating = clc.blue.italic(`imdb rating: ${rating}`);
-    console.log(`You're watching season ${boldSeason}, episode ${boldEp}, ${boldTitle} (${itlRating})`);
+    console.log(`You're watching ${boldShowTitle} season ${boldSeason}, episode ${boldEp}, ${boldTitle} (${itlRating})`);
 };
 
 const verifyShow = work => {
@@ -17,6 +18,7 @@ const verifyShow = work => {
     throw new Error(`${work.name} is not a TV Show, it's a ${work.constructor.name}`);
 };
 const getEpisodes = show => show.episodes();
+
 const filterBlankEpisodes = eps => eps.filter(ep => !!ep.name);
 const chooseRandomEpisode = eps => eps[eps.length * Math.random() | 0];
 const errorHandler = err => {
@@ -30,11 +32,18 @@ const errorHandler = err => {
 const [ query ] = process.argv.slice(2);
 const cli = new imdb.Client(SECRETS.imdb);
 
-cli.get({name: query || 'Seinfeld'})
-    .then(verifyShow)
-    .then(getEpisodes)
-    .then(filterBlankEpisodes)
-    .then(chooseRandomEpisode)
-    .then(displayEpisode)
-    .catch(errorHandler);
+const go = async () => {
+    try {
+        const show = await cli.get({name: query || 'Seinfeld'})
+        const verifiedShow = verifyShow(show);
+        const { title: showTitle } = verifiedShow;
+        const episodes = await getEpisodes(verifiedShow).then(filterBlankEpisodes);
+        const episode = chooseRandomEpisode(episodes)
+        displayEpisode(episode, showTitle);    
+    } catch (error) {
+        errorHandler(error);
+    }
+}
+
+go();
 
